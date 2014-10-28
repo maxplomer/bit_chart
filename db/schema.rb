fee4e922124bf8a3462b6ea1394ff3be8247ebf2
Max Plomer <maxplomer@gmail.com>
2014-10-28 18:02:01 -0400

    change to schema, merging to master to deploy on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028150447) do
+ActiveRecord::Schema.define(version: 20141028203221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20141028150447) do
     t.integer  "company_id", null: false
     t.float    "price",      null: false
     t.float    "change",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "trades", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "company_id", null: false
+    t.integer  "num_shares", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
