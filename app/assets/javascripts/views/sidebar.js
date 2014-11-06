2666eba950918ea5b5e3fa127e1f44244f6afde8
Max Plomer <maxplomer@gmail.com>
2014-11-05 22:11:02 -0500

    added get quote form that alerts when click

--- app/assets/javascripts/views/sidebar.js
+++ app/assets/javascripts/views/sidebar.js
@@ -4,10 +4,18 @@ FinanceClone.Views.Sidebar = Backbone.View.extend({
 
   //will later need a listenTo for the recent quotes collection and notification messages collection
 
+  events: {
+    "submit .quote-form": "getQuote"
+  },
+
   render: function () {
     var renderedContent = this.template({});
     this.$el.html(renderedContent);
     return this;
-  }
+  },
 
+  getQuote: function (event) {
+  	event.preventDefault();
+  	alert("gimme quote!")
+  }
 });
